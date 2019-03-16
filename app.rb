d8de433da51cf6f59afe08458e39e88c8742d49c
Whitny Edwards <wne4e2@gmail.com>
2019-03-16 02:03:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,16 +6,16 @@ get '/newteam' do
     erb :newteam
   end
 
-  # post '/team' do
-  #   @name = params["name"]
-  #   @coach = params["coach"]
-  #   @pg = params["pg"]
-  #   @sg = params["sg"]
-  #   @sf = params["sf"]
-  #   @pf = params["pf"]
-  #   @center = params["c"]
+  post '/team' do
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @center = params["c"]
 
-  #   erb :team
-  # end
+    erb :team
+  end
 
 end
